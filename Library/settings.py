31b60b022690150ebbb13ef3b5735275388281d9
Konrad B <50596942+conradylx@users.noreply.github.com>
2021-07-31 13:35:44 +0200

    Initial

--- Library/settings.py
+++ Library/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'django-insecure-+7dif$()cof01^nb=%i3=sb0gglv-@auy9$^4!1pf8kb0ycnvn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://agile-woodland-10718.herokuapp.com']
 
 # Application definition
 
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -117,3 +118,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+import dj_database_url
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
