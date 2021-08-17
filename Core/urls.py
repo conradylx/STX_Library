199ecb0e9961e6706b5b5db009d222001a9ebd0d
Konrad B <50596942+conradylx@users.noreply.github.com>
2021-08-17 14:23:40 +0200

    Update urls.py

--- Core/urls.py
+++ Core/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('', views.display_data, name="index"),
     path('new_book/', views.operations_on_book_function, name='add_book'),
     path('new_book/<int:id>/', views.operations_on_book_function, name='edit_book'),
-    path('import_books/', views.import_books_from_google_api, name='import_books'),
+    path('import_books/', views.fetch_books, name='import_books'),
     path('api/', ApiViewSet.as_view(), name='api_view')
 ]
